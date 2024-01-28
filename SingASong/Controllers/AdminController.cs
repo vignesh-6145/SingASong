668cd8757322535fdb0c9e31e2033b80629622c0
bandla.vignesh <vignesh.bandla01@gmail.com>
2024-01-28 23:06:13 +0530

    Added Reports page

---
+++ SingASong/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace SingASong.Controllers
+{
+    public class Admin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Reports()
+        {
+            return View();
+        }
+    }
+}
