4a82f96cf9f1afecc1e83053afab9e5ea83995f6
bandla.vignesh <vignesh.bandla01@gmail.com>
2024-01-28 13:42:43 +0530

    Modified User Page

--- SingASong/Controllers/HomeController.cs
+++ SingASong/Controllers/HomeController.cs
@@ -23,6 +23,7 @@ namespace SingASong.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
