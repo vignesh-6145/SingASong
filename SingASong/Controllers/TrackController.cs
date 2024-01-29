a36b483d4a7c7808ad013df1f59f1a554739d877
patteela.sumanth <patteelasumanth@gmail.com>
2024-01-29 09:34:04 +0530

    This is the page for individual song information.

---
+++ SingASong/Controllers/TrackController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SingASong.Controllers
+{
+    public class InfoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
