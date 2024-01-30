75dbe3024ab3fa05574016070447c1d95bf99cd9
bandla.vignesh <vignesh.bandla01@gmail.com>
2024-01-30 10:28:19 +0530

    nm

---
+++ DataService/Client/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataService.Client
+{
+    public class Client
+    {
+        public string ConnectionString()
+        {
+            return @"Integrated Security=SSPI;Initial SingASongSource=C1-BPFK114-L\\SQLEXPRESS;";
+        }
+        
+    }
+}
